a068e3d0cec012954dd5f327121ffc69085e006c
Viktor Khankhatov <hanhatov@mail.ru>
2022-06-24 17:49:19 +0800

    init

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `
+            @import "@/assets/scss/_functions.scss";
+            @import "@/assets/scss/_variables.scss";
+            @import "@/assets/scss/_mixins.scss";
+            @import "@/assets/scss/_generics.scss";
+        `
+      }
+    }
+  }
+})
